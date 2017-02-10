27af29fcad94af7a184a2360b0ca919849e20767
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-02-10 17:14:09 +0100

    return reverse echo word

--- Ohce/OhceShould.cs
+++ Ohce/OhceShould.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace Ohce
 {
@@ -15,20 +17,31 @@ namespace Ohce
         //When you introduce a palindrome, ohce likes it and after reverse-echoing it, it adds ¡Bonita palabra!
         //ohce knows when to stop, you just have to write Stop! and it'll answer Adios < your name > and end.
 
-//        $ ohce Pedro
-//> ¡Buenos días Pedro!
-//$ hola
-//> aloh
-//$ oto
-//> oto
-//> ¡Bonita palabra!
-//$ stop
-//> pots
-//$ Stop!
-//> Adios Pedro
+        //        $ ohce Pedro
+        //> ¡Buenos días Pedro!
+        //$ hola
+        //> aloh
+        //$ oto
+        //> oto
+        //> ¡Bonita palabra!
+        //$ stop
+        //> pots
+        //$ Stop!
+        //> Adios Pedro
 
-
+        [Fact]
+        public void return_reserve_word()
+        {
+            var interpreter = new OhceInterpreter();
+            interpreter.Echo("sergio").Should().Be("oigres");
+        }
     }
 
-
+    public class OhceInterpreter
+    {
+        public string Echo(string value)
+        {
+            return new string(value.Reverse().ToArray());
+        }
+    }
 }
