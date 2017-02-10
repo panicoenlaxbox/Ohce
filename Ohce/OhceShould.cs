94ca3fdf563f279199f24ccceaa6ff5decbfbe58
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-02-10 18:27:13 +0100

    greet at night, TDD on-fire

--- Ohce/OhceShould.cs
+++ Ohce/OhceShould.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using NSubstitute;
 using Xunit;
 
 namespace Ohce
 {
     public class OhceShould
     {
+        private readonly IConsole _console = Substitute.For<IConsole>();
+        private readonly ITimeProvider _timeProvider = Substitute.For<ITimeProvider>();
         //return the reverse echo of the word
         //When you introduce a palindrome, ohce likes it and after reverse-echoing it, it adds ¡Bonita palabra!
         //ohce knows when to stop, you just have to write Stop! and it'll answer Adios < your name > and end.
+
+
         //Between 20 and 6 hours, ohce will greet you saying: ¡Buenas noches < your name >!
         //Between 6 and 12 hours, ohce will greet you saying: ¡Buenos días<your name >!
         //Between 12 and 20 hours, ohce will greet you saying: ¡Buenas tardes < your name >!
@@ -33,50 +38,74 @@ namespace Ohce
         public void return_reserve_word()
         {
             var interpreter = GivenAnOhceInterpreter();
-            interpreter.Echo("sergio").Should().Be("oigres");
+            interpreter.Echo("sergio");
+            _console.Received(1).Write("oigres");
         }
 
         [Fact]
         public void return_greet_and_reserve_with_palindrome_word()
         {
             var interpreter = GivenAnOhceInterpreter();
-            interpreter.Echo("ana").Should().Be("ana\n¡Bonita palabra!");
+            interpreter.Echo("ana");
+            _console.Received(1).Write("ana\n¡Bonita palabra!");
         }
 
         [Fact]
         public void return_stop_and_bye_your_name()
         {
-            var interpreter = GivenAnOhceInterpreter(name:"Pedro");
-            interpreter.Echo("Stop!").Should().Be("Adios Pedro");
+            var interpreter = GivenAnOhceInterpreter(name: "Pedro");
+            interpreter.Echo("Stop!");
+            _console.Received(1).Write("Adios Pedro");
         }
 
-        private static OhceInterpreter GivenAnOhceInterpreter(string name="anyName")
+        [Fact]
+        public void greet_at_night()
         {
-            return new OhceInterpreter(name);
+            _timeProvider.GetCurrentTime().Returns(new DateTime(2017, 02, 10, 20, 30, 00));
+            GivenAnOhceInterpreter(name: "Pedro");
+            _console.Received(1).Write("Buenas noches Pedro");
         }
+
+        private OhceInterpreter GivenAnOhceInterpreter(string name = "anyName")
+        {
+            return new OhceInterpreter(name, _console, _timeProvider);
+        }
+    }
+
+    public interface ITimeProvider
+    {
+        DateTime GetCurrentTime();
+    }
+
+    public interface IConsole
+    {
+        void Write(string value);
     }
 
     public class OhceInterpreter
     {
-        private string _name;
+        private readonly string _name;
+        private IConsole _console;
 
-        public OhceInterpreter(string name)
+        public OhceInterpreter(string name, IConsole console, ITimeProvider timeProvider)
         {
+            _console = console;
             _name = name;
+            console.Write($"Buenas noches {name}");
         }
 
-        public string Echo(string value)
+        public void Echo(string value)
         {
             if (value == "Stop!")
             {
-                return $"Adios {_name}";
+                _console.Write($"Adios {_name}");
             }
             var reverse = new string(value.Reverse().ToArray());
             if (value == reverse)
             {
-                return $"{reverse}\n¡Bonita palabra!";
+                _console.Write($"{reverse}\n¡Bonita palabra!");
             }
-            return reverse;
+            _console.Write(reverse);
         }
     }
 }
