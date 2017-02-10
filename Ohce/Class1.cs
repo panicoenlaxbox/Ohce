8d0082d5f36cb14717ff55cbdf1552b5ee2d3521
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-02-10 17:05:39 +0100

    Add project files.

---
+++ Ohce/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ohce
+{
+    public class OhceShould
+    {
+        // return the reverse echo of the word
+        // Between 20 and 6 hours, ohce will greet you saying: ¡Buenas noches < your name >!
+        //Between 6 and 12 hours, ohce will greet you saying: ¡Buenos días<your name >!
+        //Between 12 and 20 hours, ohce will greet you saying: ¡Buenas tardes < your name >!
+        //When you introduce a palindrome, ohce likes it and after reverse-echoing it, it adds ¡Bonita palabra!
+        //ohce knows when to stop, you just have to write Stop! and it'll answer Adios < your name > and end.
+
+//        $ ohce Pedro
+//> ¡Buenos días Pedro!
+//$ hola
+//> aloh
+//$ oto
+//> oto
+//> ¡Bonita palabra!
+//$ stop
+//> pots
+//$ Stop!
+//> Adios Pedro
+
+
+    }
+
+
+}
